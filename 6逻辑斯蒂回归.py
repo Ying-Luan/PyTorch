51f7304abfa70effd5c2ee836cb02220f20b90dc
Ying-Luan <3433800035@qq.com>
2024-11-23 19:58:42 +0800

    Add files via upload

---
+++ 6逻辑斯蒂回归.py
@@ -0,0 +1,5 @@
+import torchvision
+
+train_set = torchvision.datasets.MNIST(root='../dataset/mnist', train=True, download=True)
+test_set = torchvision.datasets.MNIST(root='../dataset/mnist', train=False, download=True)
+print(train_set)
